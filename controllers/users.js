2a79cc4917d8763ebcabe74c487422f687cab476
eliasdiz <114044365+eliasdiz@users.noreply.github.com>
2024-07-30 16:09:30 -0500

    Elias (#11)
    * User: crear usuario, ruta, controlador y middlewares necesarios
    
    * User: inicio de sesion con los middleware necesarios
    
    * controlador para traer todos los usuarios
    
    * USUARIOS: controladores para editar el usuario de cliente a barbero, buscar y filtrar usuarios, sean barberos o clientes

--- controllers/users.js
+++ controllers/users.js
@@ -7,6 +7,7 @@ const controller = {
     crear: async(req,res,next) => {
         req.body.admin = false
         req.body.foto = ''
+        req.body.barbero = false
         req.body.password = bcryptjs.hashSync(req.body.password,10)
         try {
             await User.create(req.body)
@@ -36,6 +37,83 @@ const controller = {
                 })
         } catch (error) {
             next(error)
+        }
+    },
+
+    getAll: async(req,res,next) => {
+        const query = {}
+        if(req.query.parametro) query.parametro = req.query.parametro
+        try {
+            if(query.parametro === 'barberos'){
+                let query = {}
+                if (req.query.nombres){
+                    query.$or = [
+                        { nombres: {  $regex: new RegExp( req.query.nombres,'i') }},
+                        { apellidos: {  $regex: new RegExp( req.query.nombres,'i') } },
+                        {
+                            $and: [
+                                { nombres: { $regex: new RegExp(req.query.nombres.split(' ').join('|'), 'i') } },
+                                { apellidos: { $regex: new RegExp(req.query.nombres.split(' ').join('|'), 'i') } }
+                            ]
+                        }
+                    ]
+                }
+                let usuarios = await User.find({barbero: true, ...query}).sort({nombres: 1})
+                return res
+                    .status(200)
+                    .json({ usuarios})
+            }else if(query.parametro === 'clientes'){
+                let query = {}
+                if (req.query.nombres){
+                    query.$or = [
+                        { nombres: {  $regex: new RegExp( req.query.nombres,'i') }},
+                        { apellidos: {  $regex: new RegExp( req.query.nombres,'i') } },
+                        {
+                            $and: [
+                                { nombres: { $regex: new RegExp(req.query.nombres.split(' ').join('|'), 'i') } },
+                                { apellidos: { $regex: new RegExp(req.query.nombres.split(' ').join('|'), 'i') } }
+                            ]
+                        }
+                    ]
+                }
+                let usuarios = await User.find({barbero: false, ...query}).sort({nombres: 1})
+                return res 
+                    .status(200)
+                    .json({ usuarios})
+            }else{
+                let query = {}
+                if (req.query.nombres){
+                    query.$or = [
+                        { nombres: {  $regex: new RegExp( req.query.nombres,'i') }},
+                        { apellidos: {  $regex: new RegExp( req.query.nombres,'i') } },
+                        {
+                            $and: [
+                                { nombres: { $regex: new RegExp(req.query.nombres.split(' ').join('|'), 'i') } },
+                                { apellidos: { $regex: new RegExp(req.query.nombres.split(' ').join('|'), 'i') } }
+                            ]
+                        }
+                    ]
+                }
+                let usuarios = await User.find(query).sort({nombres: 1})
+                return res
+                    .status(200)
+                    .json({ usuarios})
+            }
+        } catch (error) {
+            next(error)
+        }
+    },
+
+    editarRol: async(req,res,next) => {
+        try {
+            await User.findByIdAndUpdate(req.params.id,req.body)
+            return res
+            .status(200)
+            .json({
+                    message: 'usuario actualizado',
+                });
+        } catch (error) {
+            next(error);
         }
     }
 }
--- routes/users.js
+++ routes/users.js
@@ -4,12 +4,14 @@ import usuarioExisteCrear from '../middlewares/auth/usuarioExisteCrear.js'
 import usuarioExistenteInicio from '../middlewares/auth/usuarioExisteInicio.js'
 import passwordOk from '../middlewares/auth/passwordOk.js'
 
-const { crear, inicioSesion } = controllers
+const { crear, inicioSesion, getAll, editarRol } = controllers
 
 
 const router = express.Router()
 
 router.post('/crear', usuarioExisteCrear, crear)
 router.post('/inicio-sesion', usuarioExistenteInicio, passwordOk, inicioSesion)
+router.get('/usuarios', getAll )
+router.put('/usuarios/rol/:id', editarRol)
 
 export default router
