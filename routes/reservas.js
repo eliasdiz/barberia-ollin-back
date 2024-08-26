4a1e269f9797151fc4e02730c09157eec6316825
eliasdiz <114044365+eliasdiz@users.noreply.github.com>
2024-08-26 02:27:16 -0500

    Elias (#27)
    * Usuarios: schema de validacion para edicion de usuario, actualizacion del controlador
    
    * fix editar usuario, comparar clave encriptada
    
    * Reservas-Servicios: modelo, controladores(crear y traer todos) y rutas
    
    * Reservas: controlador, ruta y schema para validar email

--- routes/reservas.js
+++ routes/reservas.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import controller from '../controllers/reservas.js'
+import validator from '../middlewares/validator.js'
+import schemaValidacion from '../Schemas/validarEmail.js'
 
-const { crear } = controller
+const { crear, validarEmail } = controller
 
 const router = express.Router()
 
 router.post('/crear',crear)
+router.post('/validar-email', validator(schemaValidacion), validarEmail )
 
 export default router
