5f4a100839717627480f68217a9acc24e6702e40
eliasdiz <114044365+eliasdiz@users.noreply.github.com>
2024-09-12 11:41:33 -0500

    Reservas: eliminar reserva por parte del usuario (#33)

--- routes/reservas.js
+++ routes/reservas.js
@@ -3,12 +3,13 @@ import controller from '../controllers/reservas.js'
 import validator from '../middlewares/validator.js'
 import schemaValidacion from '../Schemas/validarEmail.js'
 
-const { crear, validarEmail, getClientes } = controller
+const { crear, validarEmail, getClientes, eliminarReservaUsuario } = controller
 
 const router = express.Router()
 
 router.post('/crear',crear)
 router.post('/validar-email', validator(schemaValidacion), validarEmail )
 router.get('/clientes/:id', getClientes )
+router.delete('/:id', eliminarReservaUsuario)
 
 export default router
