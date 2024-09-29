700f6696a36bcf9c7ecd1b79d95b33301c3e6f82
elias <eliasj.diz@gmail.com>
2024-09-29 15:19:29 -0500

    Servicios: autenticacion con passport para crear servicios, pequeña modificiacion en el esquema de validacion

--- routes/servicios.js
+++ routes/servicios.js
@@ -2,12 +2,13 @@ import express from 'express'
 import controller from '../controllers/servicios.js'
 import validator from '../middlewares/validator.js'
 import schema from '../Schemas/CrearServicio.js'
+import passport from '../middlewares/auth/passport.js'
 
 const { crear, getTodos, eliminar, editar } = controller
 
 const router = express.Router()
 
-router.post('/crear', validator(schema),crear)
+router.post('/crear',passport.authenticate('jwt',{session:false}), validator(schema),crear)
 router.get('/',getTodos)
 router.delete('/:id', eliminar)
 router.put('/:id', validator(schema), editar)
