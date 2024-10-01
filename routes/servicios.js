dcbc26572376971023a4082a1a3a1a6d11725b4d
elias <eliasj.diz@gmail.com>
2024-09-30 23:49:01 -0500

    Servicios: controlador para editar/actualizar servicios

--- routes/servicios.js
+++ routes/servicios.js
@@ -4,14 +4,15 @@ import validator from '../middlewares/validator.js'
 import schema from '../Schemas/CrearServicio.js'
 import passport from '../middlewares/auth/passport.js'
 
-const { crear, getTodos, eliminar, editar } = controller
+const { crear, getTodos, eliminar, editar, getUno } = controller
 
 const router = express.Router()
 
 router.post('/crear',passport.authenticate('jwt',{session:false}), validator(schema),crear)
 router.get('/',getTodos)
+router.get('/servicio/:id',passport.authenticate('jwt',{session:false}), getUno)
 router.delete('/:id', eliminar)
-router.put('/:id', validator(schema), editar)
+router.put('/:id',passport.authenticate('jwt',{session:false}), validator(schema), editar)
 
 
 export default router
