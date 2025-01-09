4082b0b7a69c047fad4f45cc757548d8ac80b006
eliasdiz <114044365+eliasdiz@users.noreply.github.com>
2025-01-09 10:49:01 -0500

    Elias (#81)
    * Usuarios: schema de validacion para edicion de usuario, actualizacion del controlador
    
    * fix editar usuario, comparar clave encriptada
    
    * Reservas-Servicios: modelo, controladores(crear y traer todos) y rutas
    
    * Reservas: controlador, ruta y schema para validar email
    
    * Reservas: get cliente modificacion del controlador
    
    * Reservas: controlador y ruta para traer las reservas de cliente
    
    * Reservas: controlador y ruta para traer las reservas de cada barbero
    
    * Reservas: controlador y ruta para traer reservas de cada barbero
    
    * Reservas: modificar controlador getReservasClientes, para que me traiga solo las reservas del dia de hoy en adelante
    
    * Reservas Barbero: modificar controlador para ordanera las reservas por orden ascendente por fecha
    
    * controlador getReservasBarbero se popula por cliente_id y servicio_id
    
    * Reservas: modificacion del controlador y el modelo de la reserva. estado por activa
    
    * Reservas Actualizar estado: controlador que actualiza el estado de una reserva y crea un ingreso en el modelo IngresosBarberos
    
    * Ingresos: controlador y ruta para traer ingresos de cada babero
    
    * reservas clientes fix error
    
    * Ingresos: controlador, ruta y modelo. filtro por fechas
    
    * Email de bienvenida: boton con ruta para hacer reservas
    
    * Reservas: cambio en el controlador, ya que no se va a popular los servicio desde servicio_id. ahora es un array con los servicio seleccionados
    
    * fix quitar console.log() activos
    
    * Reservas: cambios en los controladores clientes y barbero
    
    * Reservas: modificar modelo para que acepte una hora de inicio y una final y controlador getReservasBarbero
    
    * Corregir Reservas CLiente: no trae las reservas de la DB debido a los cambios hechos
    
    * Reservas: modificacion controlador actualizar estado
    
    * Servicios: controladores eliminar y actualizar, rutas y schema de validacion
    
    * Servicios: autenticacion con passport para crear servicios, pequeña modificiacion en el esquema de validacion
    
    * Servicios: controlador para editar/actualizar servicios
    
    * Carrito: modelo ruta y controladores (crear y traer todos)
    
    * Carrito: ruta, modelo y controladores(crear, traer todos)
    
    * schema de validacion para crear productos
    
    * query para filtrar prodcutos por descripcion
    
    * Productos: middleware crear producto
    
    * Servicios: middleware para crear servicios

--- routes/productos.js
+++ routes/productos.js
@@ -3,14 +3,14 @@ import controller from '../controllers/productos.js'
 import passport from '../middlewares/auth/passport.js'
 import validator from '../middlewares/validator.js'
 import schemaCrear from '../Schemas/crearProducto.js'
-import ProductoExiste from '../middlewares/crearProducto.js'
+import productoExiste from '../middlewares/crearProducto.js'
 
 
 const { crear, getTodos} = controller
 
 const router = express.Router()
 
-router.post('/', passport.authenticate('jwt',{session:false}), validator(schemaCrear), ProductoExiste,crear)
+router.post('/', passport.authenticate('jwt',{session:false}), validator(schemaCrear), productoExiste,crear)
 router.get('/', passport.authenticate('jwt',{session:false}),getTodos)
 
 export default router
--- routes/servicios.js
+++ routes/servicios.js
@@ -3,12 +3,13 @@ import controller from '../controllers/servicios.js'
 import validator from '../middlewares/validator.js'
 import schema from '../Schemas/CrearServicio.js'
 import passport from '../middlewares/auth/passport.js'
+import crearServicio from '../middlewares/crearServicio.js'
 
 const { crear, getTodos, eliminar, editar, getUno } = controller
 
 const router = express.Router()
 
-router.post('/crear',passport.authenticate('jwt',{session:false}), validator(schema),crear)
+router.post('/crear',passport.authenticate('jwt',{session:false}), validator(schema), crearServicio,crear)
 router.get('/',getTodos)
 router.get('/servicio/:id',passport.authenticate('jwt',{session:false}), getUno)
 router.delete('/:id', eliminar)
